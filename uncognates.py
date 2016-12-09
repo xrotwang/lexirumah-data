1605827d382ff4893ed60806243cd23b8b3e84eb
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2016-12-09 15:01:24 +0100

    Give clean log of objects encountered

--- uncognates.py
+++ uncognates.py
@@ -1,26 +1,50 @@
+"""Add or report cognate coding changes in word lists."""
 import pandas
+import difflib
 
 
 import argparse
-parser = argparse.ArgumentParser(description="Add or report cognate codings in word lists")
-parser.add_argument("cognate_file", help="The file containing the cognate codes, in EDICTOR format",
-                    default="tap-alignment-merged.tsv", nargs="?")
-parser.add_argument("--derived_cognate_file", help="Other files containing the cognate codes, in EDICTOR format",
-                    default=[], nargs="*")
-parser.add_argument("--print", help="Show all differences", action="store_true", default=False)
-parser.add_argument("--save", help="Write all differences back to word lists", action="store_true", default=False)
-parser.add_argument("--split", help="Split cognate classes that cross concept boundaries",
-                    action="store_true", default=False)
+parser = argparse.ArgumentParser(
+    description="Add or report cognate codings in word lists")
+parser.add_argument(
+    "cognate_file",
+    help="The file containing the cognate codes, in EDICTOR format",
+    default="tap-alignments-merged.tsv", nargs="?")
+parser.add_argument(
+    "--derived_cognate_file",
+    help="Other files containing the cognate codes, in EDICTOR format",
+    default=[], nargs="*")
+parser.add_argument(
+    "--print",
+    help="Show all differences", action="store_true", default=False)
+parser.add_argument(
+    "--save",
+    help="Write all differences back to word lists",
+    action="store_true", default=False)
+parser.add_argument(
+    "--split",
+    help="Split cognate classes that cross concept boundaries",
+    action="store_true", default=False)
+parser.add_argument(
+    "--alignments",
+    help="Inspect and deal with alignments",
+    action="store_true", default=False)
+parser.add_argument(
+    "--bad-cogids", nargs="*",
+    help="Untrusted cogids in COGNATE_FILE, which should be ignored")
 args = parser.parse_args()
 file = args.cognate_file
 
 cognates = pandas.read_csv(
     file,
     index_col=["CONCEPT", "DOCULECT_ID", "VALUE"],
+    na_values="",
+    keep_default_na=False,
     sep="\t")
 
 cognates["COGID"] = cognates["COGID"].astype('str')
 
+# Generate form-based (and therefore more stable) cognate IDs
 for cogid, entries in cognates.groupby("COGID"):
     code = entries.index[0]
     for index in entries.index:
@@ -34,13 +58,17 @@ for cogid, entries in cognates.groupby("COGID"):
 
 cognates.sort_index(inplace=True)
 
+# Remove duplicate Concept-Language-Form combinations
 while cognates.index.duplicated().any():
     cognates = cognates[~cognates.index.duplicated()]
 
+# Read the derived cognate codings into other columns of the same data frame
 for file in args.derived_cognate_file:
     df = pandas.read_csv(
         file,
         index_col=["CONCEPT", "DOCULECT_ID", "VALUE"],
+        na_values="",
+        keep_default_na=False,
         sep="\t")
     while df.index.duplicated().any():
         df = df[~df.index.duplicated()]
@@ -56,17 +84,24 @@ for file in args.derived_cognate_file:
 
 cognates.sort_index(inplace=True)
 
+# Write cognate codings back to original word lists
+
 word_lists = {}
+# Cache opened word list data frames
+
 for i, line in cognates.iterrows():
     original_file = line["ORIGINAL_FILE"]
     word_list = word_lists.get(original_file)
 
+    # If we have not opened that word list yet, do so now.
     if word_list is None:
         try:
             word_list = pandas.read_csv(
                 original_file,
                 index_col=["English", "Language_ID", "Value"],
                 dtype={"Alignment": str},
+                na_values="",
+                keep_default_na=False,
                 sep="\t")
         except OSError:
             continue
@@ -77,6 +112,7 @@ for i, line in cognates.iterrows():
     cogid_precoding = line["COGID"]
     cogid_postcoding = line["COGID"]
 
+    # Compare what the derived cognate files did (if anything)
     for file in args.derived_cognate_file:
         if pandas.isnull(line[file]):
             continue
@@ -87,48 +123,83 @@ for i, line in cognates.iterrows():
         elif cogid_postcoding == cogid_precoding:
             cogid_postcoding = line[file]
         elif args.save:
-            raise ValueError("Original word list got changed in two different ways:",
-                             line, cogid_postcoding, line[file])
+            raise ValueError(
+                "Original word list got changed in two different ways:",
+                line, cogid_postcoding, line[file])
         else:
             print("Divergence: {:} → {:} / {:} ({:})".format(
-                    cogid_precoding, 
-                    cogid_postcoding,
-                    line[file],
-                    file))
+                cogid_precoding,
+                cogid_postcoding,
+                line[file],
+                file))
 
+    # Compare the cognate coding reflected in the word lists.
     try:
         cogid_old = word_list["Cognate Set"][i]
     except KeyError:
-        print(i)
-        continue
+        try:
+            known_forms = word_list.reset_index(level=2).loc[i[:2]]
+            sim = -1
+            for _, word in known_forms.iterrows():
+                s = difflib.SequenceMatcher(
+                    a=word["Value"],
+                    b=i[2]).ratio()
+                if s >= sim:
+                    candidate = word["Value"]
+                    sim = s
+        except AttributeError:
+            candidate = known_forms["Value"]
+        except KeyError:
+            print(
+                "Word form {:} not found in word list {:}."
+                " Assuming it got deleted".format(
+                    i, original_file))
+            continue
+        print(
+            "Word form {:} not found in word list {:}."
+            " Assuming it got corrected to {:}".format(
+                i, original_file, candidate))
+        i = i[:2] + (candidate,)
+        cogid_old = word_list["Cognate Set"][i]
     
     try:
+        # We might have duplicates in the word list. In that case,
+        # take the first instance.
         cogid_old = cogid_old.iloc[0]
     except AttributeError:
         pass
+
+    if cogid_postcoding in args.bad_cogids:
+        continue
     
     if (cogid_postcoding != cogid_old):
         word_list.set_value(i, "Cognate Set", cogid_postcoding)
         if args.print and not pandas.isnull(cogid_old):
             print(i, cogid_old, type(cogid_old), "→ ", cogid_postcoding, type(cogid_postcoding))
 
-    # Compare alignments
-    alignment_coding = line["ALIGNMENT"]
-    alignment_old = word_list["Alignment"][i]
-    
-    try:
-        alignment_old = alignment_old.iloc[0]
-    except AttributeError:
-        pass
-    
-    if (alignment_coding != alignment_old):
-        word_list.set_value(i, "Alignment", alignment_coding)
-        if args.print and not pandas.isnull(alignment_old):
-            print(i, alignment_old, "→", alignment_coding)
+    if args.alignments:
+        # Compare alignments
+        alignment_coding = line["ALIGNMENT"]
+        alignment_old = word_list["Alignment"][i]
+
+        try:
+            alignment_old = alignment_old.iloc[0]
+        except AttributeError:
+            pass
+
+        if (alignment_coding != alignment_old):
+            word_list.set_value(i, "Alignment", alignment_coding)
+            if args.print and not pandas.isnull(alignment_old):
+                print("Alignment changed: {:} ({:}) → {:} ({:})".format(
+                    alignment_old,
+                    type(alignment_old),
+                    alignment_coding,
+                    type(alignment_coding)))
 
 if args.save:
     for file, data in word_lists.items():
         word_lists[file].to_csv(
             file,
+            na_rep="",
             sep="\t")
-            
+
