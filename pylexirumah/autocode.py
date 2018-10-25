c8fdfdcd42261abaf006cb49f5791322717e6c24
Gereon Kaiping <g.a.kaiping@hum.leidenuniv.nl>
2018-10-25 17:48:40 +0200

    Some code to vary and check automatic coding

--- pylexirumah/autocode.py
+++ pylexirumah/autocode.py
@@ -9,6 +9,7 @@ import argparse
 import lingpy
 import lingpy.compare.partial
 
+from pylexirumah import get_dataset
 
 def clean_segments(row):
     """Reduce the row's segments to not contain empty morphemes.
@@ -42,30 +43,68 @@ def clean_segments(row):
     return row["Segments"]
 
 
+def clean_segments_and_rename(new_column_names):
+    def filter(row):
+        result = clean_segments(row)
+        for old, new in new_column_names.items():
+            row[new] = row.pop(old)
+        return result
+    return filter
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__.split("\n")[0])
     parser.add_argument("input", default=Path("Wordlist-metadata.json"),
                         nargs="?", type=Path,
                         help="Input file containing the CLDF word list."
                         " (default: ./Wordlist-metadata.json")
-    parser.add_argument("output", default=sys.stdout, nargs="?",
-                        type=argparse.FileType('w'),
+    parser.add_argument("output", nargs="?",
+                        # type=argparse.FileType('w'),
+                        default="aligned",
                         help="Output file to write segmented data to")
+    parser.add_argument("--threshold", default=0.55,
+                        type=float,
+                        help="Cognate clustering threshold value. (default:"
+                        " 0.55)")
+    parser.add_argument("--cluster-method", default="infomap",
+                        help="Cognate clustering method name. Valid options"
+                        " are, dependent on your LingPy version, {'upgma',"
+                        " 'single', 'complete', 'mcl', 'infomap'}."
+                        " (default: infomap)")
     args = parser.parse_args()
 
+    dataset = get_dataset(args.input)
+
+    # Explicit is better than implicit, so from_cldf does not contain renaming
+    # functionality, but it does contain a callback hook that can modify each
+    # row an filter it.
+    rename_columns = {
+         dataset["FormTable", "parameterReference"].name: "concept",
+         dataset["FormTable", "languageReference"].name: "doculect",
+         dataset["FormTable", "segments"].name: "tokens",
+         dataset["FormTable", "form"].name: "ipa",
+         dataset["FormTable", "id"].name: "reference",
+    }
+
     lex = lingpy.compare.partial.Partial.from_cldf(
-        args.input,
-        col="lect_id", row="concept_id", segments="segments", transcription="form",
-        filter=clean_segments)
-    lex.get_scorer(runs=10000)
-    lex.output('tsv', filename='lexstats', ignore=[])
+        args.input, filter=clean_segments_and_rename(rename_columns))
+
+    try:
+        scorers_etc = lingpy.compare.lexstat.LexStat("lexstats.tsv")
+        lex.scorer = scorers_etc.scorer
+        lex.cscorer = scorers_etc.cscorer
+        lex.bscorer = scorers_etc.bscorer
+    except (OSError, ValueError):
+        lex.get_scorer(runs=10000)
+        lex.output('tsv', filename='lexstats', ignore=[])
     # For some purposes it is useful to have monolithic cognate classes.
-    lex.cluster(method='lexstat', threshold=0.55, ref='cogid', cluster_method="infomap", verbose=True)
+    lex.cluster(method='lexstat', threshold=args.threshold, ref='cogid',
+                cluster_method=args.cluster_method, verbose=True)
     # But actually, in most cases partial cognates are much more useful.
-    lex.partial_cluster(method='lexstat', threshold=0.55, ref='partialids', cluster_method="infomap", verbose=True)
+    lex.partial_cluster(method='lexstat', threshold=args.threshold,
+                        cluster_method=args.cluster_method, ref='partialids',
+                        verbose=True)
     lex.output("tsv", filename="auto-clusters")
-    alm = lingpy.Alignments(lex,
-        col="lect_id", row="concept_id", segments="segments", transcription="form",
-                            ref="partialids", fuzzy=True)
+    alm = lingpy.Alignments(lex, ref="partialids", fuzzy=True)
     alm.align(method='progressive')
-    alm.output('tsv', filename='aligned', ignore='all', prettify=False)
+    alm.output('tsv', filename=args.output, ignore='all', prettify=False)
---
+++ pylexirumah/check_cognate_coding.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python
+
+"""Compare cognates in a CLDF Wordlist with a pairwise gold standard"""
+
+from pycldf.util import Path
+import argparse
+
+import csv
+from pylexirumah.util import get_dataset, cognate_sets
+
+
+def pprint_form(form_id):
+    print("{:8} {:20s} {:20s} {:s}".format(
+        forms[form_id][c_id],
+        forms[form_id][c_lect],
+        forms[form_id][c_concept],
+        " ".join(forms[form_id][c_segm])))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description=__doc__.split("\n")[0])
+    parser.add_argument("gold",
+                        type=Path,
+                        help="A CSV file listing pairs of forms and whether"
+                        " they should be considered cognates or not.")
+    parser.add_argument("--column-id-1", default="ID1",
+                        help="Header of first column containing Form_IDs in"
+                        " GOLD (default: ID1)")
+    parser.add_argument("--column-id-2", default="ID2",
+                        help="Header of second column containing Form_IDs in"
+                        " GOLD (default: ID2)")
+    parser.add_argument("--gold-column", default="Cognate",
+                        help="Header of column containing cognate judgements"
+                        " as float values between -1 [not cognate] and 1"
+                        " cognate (default: Cognate)")
+    parser.add_argument("codings", nargs="?",
+                        type=Path,
+                        help="A CLDF dataset with cognate codes")
+    parser.add_argument("--lingpy", action="store_true",
+                        default=False,
+                        help="The data is in LingPy's format, not CLDF.")
+    args = parser.parse_args()
+
+    if args.lingpy:
+        import lingpy
+        dataset = lingpy.LexStat(str(args.codings))
+        forms = {row:
+            {e: dataset[row][dataset.header[e]]
+             for e in dataset.entries
+             if e in dataset.header}
+            for row in dataset}
+        codings = {
+            form: row["partial_ids"]
+            for form, row in forms.items()}
+        c_id = "reference"
+        c_lect = "doculect"
+        c_concept = "concept"
+        c_segm = "tokens"
+    else:
+        dataset = get_dataset(args.codings)
+        cognatesets = cognate_sets(dataset)
+        codings = {
+            form: code
+            for code, forms in cognatesets.items()
+            for form in forms}
+        c_id = dataset["FormTable", "id"].name
+        c_lect = dataset["FormTable", "languageReference"].name
+        c_concept = dataset["FormTable", "parameterReference"].name
+        c_segm = dataset["FormTable", "segments"].name
+
+        forms = {row[c_id]: row for row in dataset["FormTable"].iterdicts()}
+
+    true_positives = 0
+    false_positives = 0
+    true_negatives = 0
+    false_negatives = 0
+
+    for line in csv.DictReader(args.gold.open()):
+        try:
+            judgement = float(line[args.gold_column])
+        except ValueError:
+            continue
+
+        form1 = int(line[args.column_id_1])
+        form2 = int(line[args.column_id_2])
+        c1 = codings.get(form1)
+        c2 = codings.get(form2)
+        if c1 is None or c2 is None:
+            # These forms are not coded. This is bad.
+            false_negatives += 1
+        elif judgement > 0:
+            if c1==c2:
+                # When judgement is positive, we want the cognate classes to be the same. This is fine.
+                true_positives += 1
+            else:
+                # These forms are in disagreement with the gold standard!
+                false_negatives += 1
+                print(form1, "should be cognate with", form2)
+                pprint_form(form1)
+                pprint_form(form2)
+        else:
+            if c1==c2:
+                # These forms are in disagreement with the gold standard!
+                false_positives += 1
+                print(form1, "should not be cognate with", form2)
+                pprint_form(form1)
+                pprint_form(form2)
+            else:
+                # When judgement is positive, we want the cognate classes to be the same. This is fine.
+                true_negatives += 1
+    print(true_positives, false_negatives)
+    print(false_positives, true_negatives)
+
+
